48f3cf7b81607799cfc089a193bc9dd11f739e22
Jorge Ocejo <ocejojimenezjorgeluis@gmail.com>
2020-01-27 23:39:27 -0600

    Agregue la funcionalidad de agregar nuevos grupos de servicios y la relacion de servicios

--- funciones/addrelacion.php
+++ funciones/addrelacion.php
@@ -2,12 +2,35 @@
 include("conexion.php"); 
 if ($_POST) {
 	$conexion->query("SET NAMES 'utf8'");
+	$newid_grupo_servicio=$_POST['newnamegrupo'];
 	$id_grupo_servicio=$_POST['id_grupo_servicio'];
-	$id_servicio=$_POST['id_servicio'];
-	$query="INSERT INTO relacion_servicio_grupo (id_relacion_servicio, id_servicio, id_grupo_servicio) VALUES (NULL, '$id_servicio', '$id_grupo_servicio')";
-	$resultado = $conexion->query($query) || die ("ha ocurrido un error no se guarda los datos".mysqli_error($conexion));
-	if ($resultado) {
-		echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL=../inicio.php?relacion=add">';
+	if ($newid_grupo_servicio) {
+		$query="INSERT INTO grupo_servicio (id_grupo_servicio, nombre_grupo_servicio) VALUES (NULL, '$newid_grupo_servicio')";
+		$resultado = $conexion->query($query) || die ("ha ocurrido un error no se guarda los datos".mysqli_error($conexion));
+		if (!$resultado) {
+			return false;
+		}else{
+			$id=$conexion->insert_id;
+			foreach ($_POST['servicios'] as $key => $value) {
+				$query="INSERT INTO relacion_servicio_grupo (id_relacion_servicio, id_servicio, id_grupo_servicio) VALUES (NULL, '$value', '$id')";
+				$resultado = $conexion->query($query) || die ("ha ocurrido un error no se guarda los datos".mysqli_error($conexion));
+				if (!$resultado) {
+					return false;
+				}else{
+					echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL=../inicio.php?relacion=add">';
+				}
+			}
+		}
+	}else if($id_grupo_servicio){
+		foreach ($_POST['servicios'] as $key => $value) {
+			$query="INSERT INTO relacion_servicio_grupo (id_relacion_servicio, id_servicio, id_grupo_servicio) VALUES (NULL, '$value', '$id_grupo_servicio')";
+			$resultado = $conexion->query($query) || die ("ha ocurrido un error no se guarda los datos".mysqli_error($conexion));
+			if (!$resultado) {
+				return false;
+			}else{
+				echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL=../inicio.php?relacion=add">';
+			}
+		}
 	}
 }
 ?>
