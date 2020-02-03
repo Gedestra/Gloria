9b5dc88f4a053ac42e589b46f8e81cba04ca50f1
Jorge Ocejo <ocejojimenezjorgeluis@gmail.com>
2020-02-03 15:14:36 -0600

    Se agrego la funcionalidad para poder actulizar la relacion de los sms con los tipos de actividad y guardarla en la DB

--- funciones/updatetipoactividad.php
+++ funciones/updatetipoactividad.php
@@ -14,11 +14,23 @@ if ($sincronizar==null) {
 	$addsincronizar="Inactivo";
 }else{
 	$addsincronizar="Activo";
+	if ($_POST['id_sms']!=null) {
+		$query2 = "DELETE FROM sms_relacion WHERE id_tipo_actividad= '$id_tipo_actividad'";
+		$resultado2 = $conexion->query($query2) || die ("ha ocurrido un error no se guarda los datos".mysqli_error($conexion));
+		if ($resultado2) {
+			foreach ($_POST['id_sms'] as $key => $value) {
+				$query3="INSERT INTO sms_relacion (id_relacion_sms, id_sms, id_tipo_actividad, estatus) VALUES (NULL, '$value', '$id_tipo_actividad', 'Activo')";
+				$resultado3 = $conexion->query($query3) || die ("ha ocurrido un error no se guarda los datos".mysqli_error($conexion));
+			}
+		}
+	}else{
+		$addsincronizar="Inactivo";
+	}
 }
 $query="UPDATE tipo_actividad SET nombre_tipo_actividad = '$nombre', estatus = '$addestatus', sincronizar_tipo_actividad = '$addsincronizar' WHERE id_tipo_actividad = '$id_tipo_actividad'";
 $resultado = $conexion->query($query) || die ("ha ocurrido un error no se guarda los datos".mysqli_error($conexion));
 if ($resultado) {
-		echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL=../inicio.php?tipo_actividad=update">';
+	echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL=../inicio.php?tipo_actividad=update">';
 }
 
 ?>
