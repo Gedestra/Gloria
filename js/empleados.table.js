7c4d212c04e3a542fae963274f51fbb2123e7485
Kevin Basto <bastokevindaniel@gmail.com>
2020-02-08 11:57:19 -0600

    filtros implementados

--- js/empleados.table.js
+++ js/empleados.table.js
@@ -1,11 +1,22 @@
 var HTMLtable = document.getElementById("tableBody");
 var contentTable;
-
 var table = $.ajax({
     type : "GET",
     url : "funciones/empleados.table.php",
     beforesend: function() {},
     success : res => {
+        /*res = JSON.parse(res);
+        res.forEach(element => {
+            let data = {
+                nombre: element[0],
+                apellido: element[1],
+                correo: element[2],
+                telefono: element[3],
+                sucursal: element[4],
+                sucursal: element[5]
+            }
+            console.log(data);
+        })*/
         createTable(res);
     },
     error : err => {
@@ -13,10 +24,124 @@ var table = $.ajax({
     }
 });
 
+function createTable(res){
+    res = JSON.parse(res);
+    let empleados =[];
+    res.forEach(element => {
+        let empleado = {
+            nombre: element[0],
+            apellido: element[1],
+            correo: element[2],
+            telefono: element[3],
+            sucursal: element[4],
+            estatus: element[5]
+        }
+        empleados.push(empleado);
+    });
+
+    //se guarda la informacion de los empleados en una tabla genérica para que se trate la información después
+    contentTable = empleados;
+
+    let contadorEmpleados = 0;
+    empleados.forEach(empleado => {
+        contadorEmpleados++;
+        let row = document.createElement('tr');
+        row.setAttribute('id','empleado'+contadorEmpleados);
+        let empleadoRow = {
+            nombre: document.createElement('td'),
+            correo: document.createElement('td'),
+            telefono: document.createElement('td'),
+            sucursal: document.createElement('td'),
+            estatus: document.createElement('td')
+        }
+
+        empleadoRow.nombre.innerHTML = empleado.nombre + " " + empleado.apellido;
+        empleadoRow.correo.innerHTML = empleado.correo;
+        empleadoRow.telefono.innerHTML = empleado.telefono;
+        empleadoRow.sucursal.innerHTML = empleado.sucursal;
+
+        if(empleado.estatus == 'Activo'){
+            empleadoRow.estatus.innerHTML = "<span class='btn btn-bold btn-sm btn-font-sm  btn-label-success'>"+ empleado.estatus +"</span>";
+        }else{
+            empleadoRow.estatus.innerHTML = "<span class='btn btn-bold btn-sm btn-font-sm  btn-label-danger'>"+ empleado.estatus +"</span>";
+        }
+
+        //-----------------------------------------acciones table data------------------------------------------------------------------------
+        let acciones = document.createElement('td');
+
+        let btn_group = document.createElement('div');
+        btn_group.setAttribute('class', 'btn-group');
+        btn_group.setAttribute('role', 'group');
+
+        let link = document.createElement('a');
+        link.setAttribute('href','#');
+        link.setAttribute('data-toggle','dropdown');
+        link.setAttribute('aria-haspopup','true');
+        link.setAttribute('aria-expanded','false');
+
+        let linkicon = document.createElement('i');
+        linkicon.setAttribute('class', 'la la-ellipsis-h');
+        link.appendChild(linkicon);
+
+        let drop_menu = document.createElement('div');
+        drop_menu.setAttribute('class','dropdown-menu');
+        drop_menu.setAttribute('aria-labelledby','btnGroupVerticalDrop1');
+
+        //sub menu 1
+        let sub_menu1 = document.createElement('a');
+        sub_menu1.setAttribute('class','dropdown-item');
+        sub_menu1.setAttribute('href','#');
+        sub_menu1.setAttribute('data-toggle','modal');
+        sub_menu1.setAttribute('data-target','#kt_modal_5');
+        sub_menu1.setAttribute('onclick', 'agregaform()');
+        let sub_icon1 = document.createElement('i');
+        sub_icon1.setAttribute( 'class','la la-eye');
+        sub_menu1.appendChild(sub_icon1);
+        let description1 = document.createElement('p');
+        description1.innerHTML = "ver detalles";
+        sub_menu1.appendChild(description1);
+
+        //sub menu 2
+        let sub_menu2 = document.createElement('a');
+        sub_menu2.setAttribute('class','dropdown-item');
+        sub_menu2.setAttribute('href','#');
+        sub_menu2.setAttribute('data-toggle','modal');
+        sub_menu2.setAttribute('data-target','#kt_modal_5');
+        sub_menu2.setAttribute('onclick', 'editarform()');
+        let sub_icon2 = document.createElement('i');
+        sub_icon2.setAttribute( 'class','la la-edit');
+        sub_menu2.appendChild(sub_icon2);
+        let description2 = document.createElement('p');
+        description2.innerHTML = "editar empleado";
+        sub_menu2.appendChild(description2);
+
+        drop_menu.appendChild(sub_menu1);
+        drop_menu.appendChild(sub_menu2);
+
+        btn_group.appendChild(link);
+        btn_group.appendChild(drop_menu);
+
+        acciones.appendChild(btn_group);
+
+        //-----------------------------------------acciones table data------------------------------------------------------------------------
+
+        row.appendChild(empleadoRow.nombre);
+        row.appendChild(empleadoRow.correo);
+        row.appendChild(empleadoRow.telefono);
+        row.appendChild(empleadoRow.sucursal);
+        row.appendChild(empleadoRow.estatus);
+        row.appendChild(acciones);
+
+
+
+        HTMLtable.appendChild(row);
+    });
+}
+
 //var activeFiltro = document.getElementById("activeFilter");
 //activeFiltro.setAttribute("checked",false);
 
-function createTable(res){
+/*function createTable(res){
     
     res = JSON.parse(res);
     contentTable = res;
@@ -103,7 +228,7 @@ function createTable(res){
         row.appendChild(acciones);
         HTMLtable.appendChild(row);
     })
-}
+}*/
 
 function agregaform(datos){
     console.log("Hola mundo!");
