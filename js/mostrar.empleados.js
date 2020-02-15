19b8db4e2abbf1369941ad0c4726bd8b6614923b
Jorge Ocejo <ocejojimenezjorgeluis@gmail.com>
2020-02-15 11:45:59 -0600

    se cambio la vista de disponibilidad

--- js/mostrar.empleados.js
+++ js/mostrar.empleados.js
@@ -22,7 +22,7 @@ function createEmpleados(res){
 		}
 		empleados.push(empleado);
 		let label = document.createElement('div');
-		label.innerHTML = '<input class="testradio" style="opacity: 0;width:20px;height: 20px;position: absolute;left: 22px; cursor: pointer;" type="radio" name="empleados"><span class="inputfalso" style="border-radius: 50%;display: inline-block;height: 20px;position: absolute;left: 22px;width: 21px;z-index: -1;background-color: '+empleado.color+';"></span><label style="color:#7f8581;margin-left:20px;margin-top:1px;font-size:12px;">&nbsp;'+empleado.nombre+" "+empleado.apellido+'</label>';
+		label.innerHTML = '<input style="opacity: 0;width:20px;height: 20px;position: absolute;left: 22px; cursor: pointer;" type="radio" name="empleados"><span class="inputfalso" style="border-radius: 50%;display: inline-block;height: 20px;position: absolute;left: 22px;width: 21px;z-index: -1;background-color: '+empleado.color+';"></span><label style="color:#7f8581;margin-left:20px;margin-top:1px;font-size:12px;">&nbsp;'+empleado.nombre+" "+empleado.apellido+'</label>';
 		document.getElementById('nombreempleados').appendChild(label);
 	});
 }
@@ -48,7 +48,7 @@ function createSucursal(res){
 		}
 		sucursales.push(sucursal);
 		let label = document.createElement('label');
-		label.innerHTML = '<input class="testradio2"style="opacity: 0;width:20px;height: 20px;position: absolute;left: 22px; cursor: pointer;" type="radio" name="sucursales"><span class="inputfalso" style="border-radius: 50%;display: inline-block;height: 20px;position: absolute;left: 22px;width: 21px;z-index: -1;background-color: #ccc"></span><label style="color:#7f8581;margin-left:20px;margin-top:1px;font-size:12px;">&nbsp;'+sucursal.nombre+'</label>';
+		label.innerHTML = '<input style="opacity: 0;width:20px;height: 20px;position: absolute;left: 22px; cursor: pointer;" type="radio" name="empleados"><span class="inputfalso" style="border-radius: 50%;display: inline-block;height: 20px;position: absolute;left: 22px;width: 21px;z-index: -1;background-color: #ccc"></span><label style="color:#7f8581;margin-left:20px;font-size:12px;">&nbsp;'+sucursal.nombre+'</label>';
 		document.getElementById('nombresucursales').appendChild(label);
 	});
 }
